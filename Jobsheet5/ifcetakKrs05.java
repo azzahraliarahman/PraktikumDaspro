a010642902393af3cb48464cc98ac2ac6e898fa5
azzahraliarahman <azzahraliarahma@gmail.com>
2025-10-04 22:58:58 +0700

    Tugas 1

--- Jobsheet5/ifcetakKrs05.java
+++ Jobsheet5/ifcetakKrs05.java
@@ -1,17 +1,15 @@
+
 import java.util.Scanner;
 public class ifcetakKrs05 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("---Cetak KRS SIAKAD ---");
         System.out.println("Apakah UKT Sudah Lunas? (True/False)");
-        boolean uktLunas = input.nextBoolean();
+        boolean uktLunas = sc.nextBoolean();
         
-        if (uktLunas) {
-        System.out.println("Pembayaran UKT terverikasi");
-        System.out.println("Silahkan Cetak KRS dan minta tanda tangan DPA");
-        }
-        else {
-        System.out.println("Registrasi ditolak,silakan lunasi UKT terlebih dahulu");}
+        String pesan = uktLunas ? "Pembayaran UKT terverifikasi\nSilahkan Cetak KRS dan minta tanda tangan DPA":"Registrasi ditolak, silakan lunasi UKT terlebih dahulu";
+        System.out.println(pesan);
+        sc.close();
     }
 }
