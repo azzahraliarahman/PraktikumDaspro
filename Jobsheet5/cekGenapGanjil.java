1b8b95f65a6089d2ce3cd32630bb0cada432ede8
azzahraliarahman <azzahraliarahma@gmail.com>
2025-10-04 23:16:49 +0700

    Tugas 2

---
+++ Jobsheet5/cekGenapGanjil.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class cekGenapGanjil {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+        String hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
+        System.out.println(angka + " adalah bilangan " + hasil);
+    }
+    
+}
