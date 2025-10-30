620ae74c60dea82156228eb8e4d512e21851c031
azzahraliarahman <azzahraliarahma@gmail.com>
2025-10-30 21:50:51 +0700

    Percobaan 3

---
+++ jobsheet7/jobsheet8/Triangle05.java
@@ -0,0 +1,22 @@
+package jobsheet8;
+
+import java.util.Scanner;
+
+public class Triangle05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai n = ");
+        int n = scanner.nextInt();
+        int i = 0;
+        while (i <= n) {
+            int j = 0;
+            while (j < i) {
+                System.out.print("*");
+                j++;
+            }
+            i++;
+        }
+        
+    }
+}
